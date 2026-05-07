151b54a046b8c669bd7930dd9d6fa8255341a564
henriqueoshikirigomes-blip <henrique.oshikiri.gomes@escola.pr.gov.br>
2026-05-07 08:45:35 -0300

    a

--- script.js
+++ script.js
@@ -58,8 +58,8 @@ window.onload = function () {
 function excluirCarro(id) {
     let carros = JSON.parse(localStorage.getItem('carros')) || [];
 
-    carros = carros.filter(carro => carro.id Number() !==Number id);
-    
+    carros = carros.filter(carro => Number(carro.id) !== Number(id));
+
     localStorage.setItem('carros', JSON.stringify(carros));
 
     document.getElementById('listaCarros').innerHTML = "";
